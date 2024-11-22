9f0b6395e584c1902d4f7fbb122fffe3d2a3b9ff
Hugo Janssen <janssenlive@gmail.com>
2024-11-22 15:44:02 -0600

    Implementadas funciones de parsear lenguaje, eleminiar epsilons y eliminar unitarios

--- FormaNormal/urls.py
+++ FormaNormal/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from FormaNormal.views import mainView
 
 urlpatterns = [
-    path('', admin.site.urls)
+    path('', mainView, name="main"),
 ]
--- FormaNormal/views.py
+++ FormaNormal/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
 def mainView(request):
+    if request.method == "POST":
+        lenguaje = request.POST['lenguaje']
+        print(lenguaje)
     context = {}
-    return render(request, "html", context)
+    return render(request, "new.html", context)
